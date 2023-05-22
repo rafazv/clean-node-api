c85d688a8a62207cd33e532ea0145f3ff1da028a
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-05-22 17:28:37 -0400

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,9 @@
-/* eslint-disable semi */
-/* eslint-disable indent */
-/* eslint-disable no-tabs */
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
 module.exports = {
-	roots: ["<rootDir>/src"],
-	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-	coverageDirectory: "coverage",
-	testEnvironment: "node",
-	transform: {
-		".+\\.ts$": "ts-jest",
-	},
-};
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
