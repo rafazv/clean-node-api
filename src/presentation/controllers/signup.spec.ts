e4f654871aa52fbfd7bf91e8f2b568147d085a36
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-05-30 15:15:06 -0400

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUpController', () => {
     expect(httpRes.statusCode).toBe(400)
     expect(httpRes.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpReq = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpRes = sut.handle(httpReq)
+    expect(httpRes.statusCode).toBe(400)
+    expect(httpRes.body).toEqual(new Error('Missing param: email'))
+  })
 })
