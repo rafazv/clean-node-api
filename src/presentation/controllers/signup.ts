86a7bfa94f9ea46e87271f4905a9366a2ae9a852
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-05-30 15:14:57 -0400

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,17 @@
 export class SignUpController {
   handle (httpReq: any): any {
-    return { statusCode: 400, body: new Error('Missing param: name') }
+    if (!httpReq.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpReq.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
+    }
   }
 }
