ed3bd78b9147461639316233cc428e31195d5b36
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-05-31 13:59:50 -0400

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -96,4 +96,20 @@ describe('SignUpController', () => {
     expect(httpRes.statusCode).toBe(400)
     expect(httpRes.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpReq = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpReq)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
