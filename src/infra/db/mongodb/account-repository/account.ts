c37d463f47c742d3d29bcbf079eac01252a9251f
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-06-26 17:06:58 -0400

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,7 +3,7 @@ import { MongoHelper } from './helpers/mongo-helper'
 
 export class AccountMongoRepository {
   async add (accountData: AddAccountModel): Promise<boolean> {
-    const accountCollection = MongoHelper.getConnection('accounts')
+    const accountCollection = await MongoHelper.getConnection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return result.insertedId !== null
   }
