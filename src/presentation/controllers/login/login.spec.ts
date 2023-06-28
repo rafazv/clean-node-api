a32ac7bb0a10b97cefcc3a742f1f6c9a1fdf0fd0
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-06-28 15:01:30 -0400

    test: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -13,4 +13,15 @@ describe('Login Controller', () => {
     const httpRes = await sut.handle(httpReq)
     expect(httpRes).toEqual(badRequest(new MissingParamError('email')))
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new LoginController()
+    const httpReq = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpRes = await sut.handle(httpReq)
+    expect(httpRes).toEqual(badRequest(new MissingParamError('password')))
+  })
 })
