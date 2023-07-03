b9fdc1364ade4c33de545dbde0cd74bb7d2ffcea
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-07-03 15:43:49 -0400

    test: ensure ValidationComposite returns an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,41 @@
+import { MissingParamError } from '../../errors'
+import { type Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  // test('Should not return if validation succeeds', () => {
+  //   const sut = makeSut()
+  //   const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+  //   expect(error).toBeFalsy()
+  // })
+})
--- src/presentation/helpers/validators/validation-composite.ts
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -8,10 +8,12 @@ export class ValidationComposite implements Validation {
   }
 
   validate (input: any): Error | null {
-    this.validations.forEach((validation) => {
+    for (const validation of this.validations) {
       const error = validation.validate(input)
-      if (error) return error
-    })
+      if (error) {
+        return error
+      }
+    }
     return null
   }
 }
