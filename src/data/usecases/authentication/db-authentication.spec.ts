621b7d04ac0a93212eac902e1aee85747c5c2185
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-07-05 15:35:34 -0400

    test: ensure DbAuthentication returns null if HashComparer returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -68,7 +68,7 @@ describe('DbAuthentication UseCase', () => {
 
   // test('Should return null if LoadAccountByEmailRepository returns null', async () => {
   //   const { sut, loadAccountByEmailRepositoryStub } = makeSut()
-  //   jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+  //   jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => { resolve(null) }))
   //   const accessToken = await sut.auth(makeFakeAuthentication())
   //   expect(accessToken).toBeNull()
   // })
@@ -85,5 +85,12 @@ describe('DbAuthentication UseCase', () => {
     jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => { resolve(false) }))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeNull()
   })
 })
