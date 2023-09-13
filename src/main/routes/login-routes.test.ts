9a2f3ccd53313d845e418a57c1c8883c803fd8ef
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-09-13 14:32:03 -0400

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/account/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,24 @@ describe('Login Routes', () => {
           email: 'rafaela@mail.com',
           password: '12345',
           passwordConfirmation: '12345'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('12345', 12)
+      await accountCollection.insertOne({
+        name: 'Rafaela',
+        email: 'rafaela@mail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rafaela@mail.com',
+          password: '12345'
         })
         .expect(200)
     })
