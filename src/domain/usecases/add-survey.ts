665eb8f90e598263fd38f4e6b73a7154abb8ae2d
Rafaela Lima <ral@icomp.ufam.edu.br>
2023-11-09 17:14:31 -0400

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+export interface SurveyAnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurvey {
+  add: (survey: AddSurveyModel) => Promise<void>
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,14 +1,20 @@
 import { badRequest } from '../../../helpers/http/http-helper'
-import { type HttpRequest, type HttpResponse, type Controller, type Validation } from './add-survey-controller-protocols'
+import { type HttpRequest, type HttpResponse, type Controller, type Validation, type AddSurvey } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpReq: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpReq.body)
     if (error) return badRequest(error)
-    return await new Promise(resolve => { resolve(null) })
+    const { question, answers } = httpReq.body
+    await this.addSurvey.add({
+      question,
+      answers
+    })
+    return { statusCode: 204, body: null }
   }
 }
